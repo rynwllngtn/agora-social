33138f246555bf6992ab7409601bff278e8a2b54
Ryan Wellington <rynwllngtn@gmail.com>
2026-03-20 16:06:17 -0300

    init

---
+++ src/test/java/dev/rynwllngtn/agorasystem/AgoraSystemSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rynwllngtn.agorasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraSystemSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
